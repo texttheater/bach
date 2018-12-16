ba5cdade00d1c60677ab52e85bdfa509a12459d2
Kilian Evang <noreply@texttheater.net>
2018-12-16 10:37:42 +0100

    Improved parameter string representation
    * `for InputType` now suppressed for simple parameters (simple parameters =
      have input type AnyType AND no parameters)
    * empty pair of parens now suppressed for parameters that don't have
      parameters, in line with grammar

--- functions/parameters.go
+++ functions/parameters.go
@@ -1,8 +1,7 @@
 package functions
 
 import (
-	"fmt"
-	"strings"
+	"bytes"
 
 	"github.com/texttheater/bach/types"
 )
@@ -33,10 +32,23 @@ func (this *Parameter) Subsumes(that *Parameter) bool {
 }
 
 func (p *Parameter) String() string {
-	paramStrings := make([]string, 0, len(p.Params))
-	for _, param := range p.Params {
-		paramStrings = append(paramStrings, param.String())
+	buffer := bytes.Buffer{}
+	if !p.InputType.Subsumes(&types.AnyType{}) || len(p.Params) > 0 {
+		buffer.WriteString("for ")
+		buffer.WriteString(p.InputType.String())
+		buffer.WriteString(" ")
 	}
-	paramsString := strings.Join(paramStrings, ",")
-	return fmt.Sprintf("for %s %s(%s) %s", p.InputType, p.Name, paramsString, p.OutputType)
+	buffer.WriteString(p.Name)
+	if len(p.Params) > 0 {
+		buffer.WriteString("(")
+		buffer.WriteString(p.Params[0].String())
+		for _, param := range p.Params[1:] {
+			buffer.WriteString(",")
+			buffer.WriteString(param.String())
+		}
+		buffer.WriteString(")")
+	}
+	buffer.WriteString(" ")
+	buffer.WriteString(p.OutputType.String())
+	return buffer.String()
 }
