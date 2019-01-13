dba54894a8bb4bea95500b4e41f8449526ea3f76
Kilian Evang <noreply@texttheater.net>
2019-01-13 21:51:16 +0100

    Added out, err funcers

--- builtin/builtin.go
+++ builtin/builtin.go
@@ -1,6 +1,9 @@
 package builtin
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/texttheater/bach/functions"
 	"github.com/texttheater/bach/types"
 	"github.com/texttheater/bach/values"
@@ -156,6 +159,34 @@ func initialShape() functions.Shape {
 				Stack: inputState.Stack,
 			}
 			return outputState
+		}
+		return nil, outputType, action, true
+	})
+	shape.FuncerStack = shape.FuncerStack.Push(func(gotInputType types.Type, gotName string, gotNumArgs int) ([]*functions.Parameter, types.Type, functions.Action, bool) {
+		if gotName != "out" {
+			return nil, nil, nil, false
+		}
+		if gotNumArgs != 0 {
+			return nil, nil, nil, false
+		}
+		outputType := gotInputType
+		action := func(inputState functions.State, args []functions.Action) functions.State {
+			fmt.Println(inputState.Value)
+			return inputState
+		}
+		return nil, outputType, action, true
+	})
+	shape.FuncerStack = shape.FuncerStack.Push(func(gotInputType types.Type, gotName string, gotNumArgs int) ([]*functions.Parameter, types.Type, functions.Action, bool) {
+		if gotName != "err" {
+			return nil, nil, nil, false
+		}
+		if gotNumArgs != 0 {
+			return nil, nil, nil, false
+		}
+		outputType := gotInputType
+		action := func(inputState functions.State, args []functions.Action) functions.State {
+			fmt.Fprintln(os.Stderr, inputState.Value)
+			return inputState
 		}
 		return nil, outputType, action, true
 	})
