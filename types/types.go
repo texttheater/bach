ccd4fb816119abd75376d532ea578b580c8bf2a9
Kilian Evang <noreply@texttheater.net>
2019-03-02 13:08:05 +0100

    Added obj type (experimental)

--- types/types.go
+++ types/types.go
@@ -3,6 +3,7 @@ package types
 import (
 	"bytes"
 	"fmt"
+	"sort"
 )
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -143,6 +144,65 @@ func (t *ArrType) String() string {
 
 func (t *ArrType) ElementType() Type {
 	return t.ElType
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+func NewObjType(propTypeMap map[string]Type) Type {
+	props := make([]string, 0, len(propTypeMap))
+	for k := range propTypeMap {
+		props = append(props, k)
+	}
+	sort.Strings(props)
+	return &objType{
+		props:       props,
+		propTypeMap: propTypeMap,
+	}
+}
+
+type objType struct {
+	props       []string
+	propTypeMap map[string]Type
+}
+
+func (t *objType) Subsumes(other Type) bool {
+	otherObjType, ok := other.(*objType)
+	if !ok {
+		return false
+	}
+	for k, v1 := range t.propTypeMap {
+		v2, ok := otherObjType.propTypeMap[k]
+		if !ok {
+			return false
+		}
+		if !v1.Subsumes(v2) {
+			return false
+		}
+	}
+	return true
+}
+
+func (t *objType) String() string {
+	buffer := bytes.Buffer{}
+	buffer.WriteString("Obj<")
+	if len(t.props) > 0 {
+		buffer.WriteString(t.props[0])
+		buffer.WriteString(": ")
+		buffer.WriteString(t.propTypeMap[t.props[0]].String())
+		for _, prop := range t.props {
+			typ := t.propTypeMap[prop]
+			buffer.WriteString(", ")
+			buffer.WriteString(prop)
+			buffer.WriteString(": ")
+			buffer.WriteString(typ.String())
+		}
+	}
+	buffer.WriteString(">")
+	return buffer.String()
+}
+
+func (t *objType) ElementType() Type {
+	panic(fmt.Sprintf("%s is not a sequence type", t))
 }
 
 ///////////////////////////////////////////////////////////////////////////////
