48fd1593e60bf8132e7632510c32fd5f4f039c30
Kilian Evang <noreply@texttheater.net>
2019-03-04 15:19:58 +0100

    Added obj values (experimental)

--- values/values.go
+++ values/values.go
@@ -130,3 +130,32 @@ func (v ArrValue) Iter() <-chan Value {
 }
 
 ///////////////////////////////////////////////////////////////////////////////
+
+type ObjValue map[string]Value
+
+func (v ObjValue) String() string {
+	var buffer bytes.Buffer
+	buffer.WriteString("{")
+	firstWritten := false
+	for k, w := range v {
+		if firstWritten {
+			buffer.WriteString(", ")
+		}
+		buffer.WriteString(k)
+		buffer.WriteString(": ")
+		buffer.WriteString(w.String())
+		firstWritten = true
+	}
+	buffer.WriteString("}")
+	return buffer.String()
+}
+
+func (v ObjValue) Out() string {
+	return v.String()
+}
+
+func (v ObjValue) Iter() <-chan Value {
+	panic(fmt.Sprintf("%s is not a sequence", v))
+}
+
+///////////////////////////////////////////////////////////////////////////////
