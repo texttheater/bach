7294e4977e2698a4b0a1faf877e44437e8425b89
Kilian Evang <noreply@texttheater.net>
2019-06-30 09:36:35 +0200

    Implemented array and object pattern matching
    Still utterly untested.

--- patterns/arr.go
+++ patterns/arr.go
@@ -2,8 +2,11 @@ package patterns
 
 import (
 	"github.com/alecthomas/participle/lexer"
+	"github.com/texttheater/bach/errors"
 	"github.com/texttheater/bach/shapes"
+	"github.com/texttheater/bach/states"
 	"github.com/texttheater/bach/types"
+	"github.com/texttheater/bach/values"
 )
 
 type ArrPattern struct {
@@ -12,5 +15,63 @@ type ArrPattern struct {
 }
 
 func (p *ArrPattern) Typecheck(inputShape shapes.Shape) (shapes.Shape, types.Type, Matcher, error) {
-	panic("not implemented")
+	// process element patterns
+	funcerStack := inputShape.Stack
+	elementTypes := make([]types.Type, len(p.ElementPatterns))
+	elementMatchers := make([]Matcher, len(p.ElementPatterns))
+	for i, elPattern := range p.ElementPatterns {
+		elShape, _, elMatcher, err := elPattern.Typecheck(shapes.Shape{
+			Type:  inputShape.Type,
+			Stack: funcerStack,
+		})
+		if err != nil {
+			return shapes.Shape{}, nil, nil, err
+		}
+		funcerStack = elShape.Stack
+		elementTypes[i] = elShape.Type
+		elementMatchers[i] = elMatcher
+	}
+	// determine the type of values this pattern will match
+	pType := types.TupType(elementTypes)
+	// partition the input type and check for impossible match
+	intersection, complement := inputShape.Type.Partition(pType)
+	if (types.VoidType{}).Subsumes(intersection) {
+		return shapes.Shape{}, nil, nil, errors.E(
+			errors.Code(errors.ImpossibleMatch),
+			errors.Pos(p.Pos),
+			errors.WantType(inputShape.Type),
+			errors.GotType(pType),
+		)
+	}
+	// build output shape
+	outputShape := shapes.Shape{
+		Type:  intersection,
+		Stack: funcerStack,
+	}
+	// build matcher
+	matcher := func(inputState states.State) (*states.VariableStack, bool) {
+		varStack := inputState.Stack
+		switch v := inputState.Value.(type) {
+		case values.ArrValue:
+			if len(v) != len(elementMatchers) {
+				return nil, false
+			}
+			for i, elMatcher := range elementMatchers {
+				var ok bool
+				varStack, ok = elMatcher(states.State{
+					Value: v[i],
+					Stack: varStack,
+				})
+				if !ok {
+					return nil, false
+				}
+			}
+			return varStack, true
+		default:
+			return nil, false
+		}
+	}
+	// return
+	return outputShape, complement, matcher, nil
+
 }
--- patterns/obj.go
+++ patterns/obj.go
@@ -2,8 +2,11 @@ package patterns
 
 import (
 	"github.com/alecthomas/participle/lexer"
+	"github.com/texttheater/bach/errors"
 	"github.com/texttheater/bach/shapes"
+	"github.com/texttheater/bach/states"
 	"github.com/texttheater/bach/types"
+	"github.com/texttheater/bach/values"
 )
 
 type ObjPattern struct {
@@ -11,6 +14,63 @@ type ObjPattern struct {
 	PropPatternMap map[string]Pattern
 }
 
-func (f *ObjPattern) Typecheck(inputShape shapes.Shape) (shapes.Shape, types.Type, Matcher, error) {
-	panic("not implemented")
+func (p *ObjPattern) Typecheck(inputShape shapes.Shape) (shapes.Shape, types.Type, Matcher, error) {
+	// process value patterns
+	funcerStack := inputShape.Stack
+	propTypeMap := make(map[string]types.Type)
+	propMatcherMap := make(map[string]Matcher)
+	for prop, valPattern := range p.PropPatternMap {
+		valShape, _, valMatcher, err := valPattern.Typecheck(shapes.Shape{
+			Type:  inputShape.Type,
+			Stack: funcerStack,
+		})
+		if err != nil {
+			return shapes.Shape{}, nil, nil, err
+		}
+		funcerStack = valShape.Stack
+		propTypeMap[prop] = valShape.Type
+		propMatcherMap[prop] = valMatcher
+	}
+	// determine the type of values this pattern will match
+	pType := types.NewObjType(propTypeMap)
+	// partition the input type and check for impossible match
+	intersection, complement := inputShape.Type.Partition(pType)
+	if (types.VoidType{}).Subsumes(intersection) {
+		return shapes.Shape{}, nil, nil, errors.E(
+			errors.Code(errors.ImpossibleMatch),
+			errors.Pos(p.Pos),
+			errors.WantType(inputShape.Type),
+			errors.GotType(pType),
+		)
+	}
+	// build output shape
+	outputShape := shapes.Shape{
+		Type:  intersection,
+		Stack: funcerStack,
+	}
+	// build matcher
+	matcher := func(inputState states.State) (*states.VariableStack, bool) {
+		varStack := inputState.Stack
+		switch v := inputState.Value.(type) {
+		case values.ObjValue:
+			for prop, valMatcher := range propMatcherMap {
+				value, ok := v[prop]
+				if !ok {
+					return nil, false
+				}
+				varStack, ok = valMatcher(states.State{
+					Value: value,
+					Stack: varStack,
+				})
+				if !ok {
+					return nil, false
+				}
+			}
+			return varStack, true
+		default:
+			return nil, false
+		}
+	}
+	// return
+	return outputShape, complement, matcher, nil
 }
