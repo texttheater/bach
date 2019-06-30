efd0903114ba378c50ee31704fb8265e5dceafe1
Kilian Evang <noreply@texttheater.net>
2019-06-30 18:10:46 +0200

    Added more array pattern tests

--- tests/matching_arr.go
+++ tests/matching_arr.go
@@ -14,7 +14,8 @@ func TestMatchingArr(t *testing.T) {
 		types.BoolType{},
 		values.BoolValue(true),
 		nil,
-		t)
+		t,
+	)
 	TestProgram(
 		`[1, 2, 3] is [Num, Num, Num] then true else false ok`,
 		nil,
@@ -22,35 +23,64 @@ func TestMatchingArr(t *testing.T) {
 		errors.E(
 			errors.Code(errors.UnreachableElseClause),
 		),
-		t)
+		t,
+	)
 	TestProgram(
 		`[1, 2, 3] each id all arr is [Num, Num, Num] then true else false ok`,
 		types.BoolType{},
 		values.BoolValue(true),
 		nil,
-		t)
+		t,
+	)
 	TestProgram(
 		`[1, "a"] is [Num, Str] then true ok`,
 		types.BoolType{},
 		values.BoolValue(true),
 		nil,
-		t)
+		t,
+	)
 	TestProgram(
 		`[1, "a"] is [Num a, Str b] then a ok`,
 		types.NumType{},
 		values.NumValue(1),
 		nil,
-		t)
+		t,
+	)
 	TestProgram(
 		`[1, "a"] is [Num a, Str b] then b ok`,
 		types.StrType{},
 		values.StrValue("a"),
 		nil,
-		t)
+		t,
+	)
 	TestProgram(
 		`[[1]] is [[Any x]] then x ok`,
 		types.NumType{},
 		values.NumValue(1),
 		nil,
-		t)
+		t,
+	)
+	TestProgram(
+		`if true then [1] else [2] ok is [Num a] then a ok`,
+		types.NumType{},
+		values.NumValue(1),
+		nil,
+		t,
+	)
+	TestProgram(
+		`if true then [1] else ["2"] ok is [Num a] then a ok`,
+		nil,
+		nil,
+		errors.E(
+			errors.Code(errors.NonExhaustiveMatch),
+		),
+		t,
+	)
+	TestProgram(
+		`if true then [1] else ["2"] ok is [Num a] then a elis [Str a] then a ok`,
+		types.Union(types.NumType{}, types.StrType{}),
+		values.NumValue(1),
+		nil,
+		t,
+	)
 }
