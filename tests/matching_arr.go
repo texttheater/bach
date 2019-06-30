307a525eafe1c076722874d650eff265b7cd40ee
Kilian Evang <noreply@texttheater.net>
2019-06-30 17:59:29 +0200

    Bugfix: had accidentally removed arr pattern tests

--- tests/matching_arr.go
+++ tests/matching_arr.go
@@ -10,51 +10,47 @@ import (
 
 func TestMatchingArr(t *testing.T) {
 	TestProgram(
-		`if true then 2 else "two" ok is Num then true else false ok`,
+		`[1, 2, 3] is [Num, Num, Num] then true ok`,
 		types.BoolType{},
 		values.BoolValue(true),
 		nil,
-		t,
-	)
+		t)
 	TestProgram(
-		`if true then 2 else "two" ok is Num then true ok`,
-		nil,
-		nil,
-		errors.E(
-			errors.Code(errors.NonExhaustiveMatch),
-			errors.WantType(types.VoidType{}),
-			errors.GotType(types.StrType{}),
-		),
-		t,
-	)
-	TestProgram(
-		`if true then 2 else "two" ok is Num then true elis Str then false ok`,
-		types.BoolType{},
-		values.BoolValue(true),
-		nil,
-		t,
-	)
-	TestProgram(
-		`if true then 2 else "two" ok is Num then true elis Str then false else false ok`,
+		`[1, 2, 3] is [Num, Num, Num] then true else false ok`,
 		nil,
 		nil,
 		errors.E(
 			errors.Code(errors.UnreachableElseClause),
 		),
-		t,
-	)
+		t)
 	TestProgram(
-		`[1, 2, 3] is Seq<Num> then each +1 all arr ok`,
-		&types.ArrType{types.NumType{}},
-		values.ArrValue([]values.Value{values.NumValue(2), values.NumValue(3), values.NumValue(4)}),
+		`[1, 2, 3] each id all arr is [Num, Num, Num] then true else false ok`,
+		types.BoolType{},
+		values.BoolValue(true),
 		nil,
-		t,
-	)
-	TestProgram( // Intersective matching: pattern says Seq<Any> but Bach knows it got Seq<Num>
-		`[1, 2, 3] is Seq<Any> then each +1 all arr ok`,
-		&types.ArrType{types.NumType{}},
-		values.ArrValue([]values.Value{values.NumValue(2), values.NumValue(3), values.NumValue(4)}),
+		t)
+	TestProgram(
+		`[1, "a"] is [Num, Str] then true ok`,
+		types.BoolType{},
+		values.BoolValue(true),
 		nil,
-		t,
-	)
+		t)
+	TestProgram(
+		`[1, "a"] is [Num a, Str b] then a ok`,
+		types.NumType{},
+		values.NumValue(1),
+		nil,
+		t)
+	TestProgram(
+		`[1, "a"] is [Num a, Str b] then b ok`,
+		types.StrType{},
+		values.StrValue("a"),
+		nil,
+		t)
+	TestProgram(
+		`[[1]] is [[Any x]] then x ok`,
+		types.NumType{},
+		values.NumValue(1),
+		nil,
+		t)
 }
