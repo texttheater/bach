0c94d7a963fc043fadaddfd3c6e003e935a9fca2
Kilian Evang <noreply@texttheater.net>
2019-08-04 09:24:20 +0200

    Added two more tests for object patterns

--- tests/matching_obj.go
+++ tests/matching_obj.go
@@ -57,4 +57,22 @@ func TestMatchingObj(t *testing.T) {
 		),
 		t,
 	)
+	TestProgram(
+		`if true then {a: 1} else {b: 2} ok`,
+		types.Union(types.NewObjType(map[string]types.Type{"a": types.NumType{}}), types.NewObjType(map[string]types.Type{"b": types.NumType{}})),
+		values.ObjValue{"a": values.NumValue(1)},
+		nil,
+		t,
+	)
+	// TODO In the following program, the type of x could be inferred in
+	// each pattern but isn't. Is this a problem? It is because our object
+	// types currently cannot represent the *absence* of attributes, so any
+	// attribute not present in the type has Any type by default.
+	TestProgram(
+		`if true then {a: 1} else {b: 2} ok is {a: Num x} with x ==1 then true elif false then false elis {a: Num x} then x elis {b: Num x} then x ok`,
+		types.Union(types.BoolType{}, types.NumType{}),
+		values.BoolValue(true),
+		nil,
+		t,
+	)
 }
