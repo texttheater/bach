bdc93dc0a7e0135ddb26c64b47a52f533fded67e
Kilian Evang <noreply@texttheater.net>
2019-08-11 09:50:36 +0200

    Revert "Syntactic support for branching mapping"
    This reverts commit e233fcc4fee5b1402b4699d8f0d0d806b1be955c.

--- grammar/mappings.go
+++ grammar/mappings.go
@@ -6,43 +6,12 @@ import (
 )
 
 type Mapping struct {
-	Pos       lexer.Position    `"each"`
-	Body      *Composition      `( @@`
-	Pattern   *Pattern          `| ( @@`
-	Guard     *Composition      `    ( "with" @@)?`
-	Condition *Composition      `  | "if" @@)`
-	LongTail  *LongMappingTail  `  ( @@`
-	ShortTail *ShortMappingTail `  | @@) ) "all"`
-}
-
-type ShortMappingTail struct {
-	Pos          lexer.Position
-	Alternatives []*ShortMappingAlternative `( @@ )*`
-}
-
-type LongMappingTail struct {
-	Pos          lexer.Position
-	Consequent   *Composition              `"then" ( @@ | "drop")`
-	Alternatives []*LongMappingAlternative `( @@ )*`
-}
-
-type ShortMappingAlternative struct {
-	Pos       lexer.Position
-	Pattern   *Pattern     `( "elis" @@`
-	Guard     *Composition `  ( "with" @@ )?`
-	Condition *Composition `| "elif" @@ )`
-}
-
-type LongMappingAlternative struct {
-	Pos        lexer.Position
-	Pattern    *Pattern     `( "elis" @@`
-	Guard      *Composition `  ( "with" @@ )?`
-	Condition  *Composition `| "elif" @@ )`
-	Consequent *Composition `"then" @@`
+	Pos  lexer.Position
+	Body *Composition `"each" @@ "all"`
 }
 
 func (g *Mapping) Ast() (functions.Expression, error) {
-	body, err := g.Body.Ast() // TODO handle other case
+	body, err := g.Body.Ast()
 	if err != nil {
 		return nil, err
 	}
