2c9aeea98d5675c84ed7624c230a300b8b9a006d
Kilian Evang <noreply@texttheater.net>
2019-09-06 01:40:53 +0200

    Added package-level test for parser

---
+++ parser/parser_test.go
@@ -0,0 +1,26 @@
+package parser_test
+
+import (
+	"github.com/alecthomas/participle"
+	"github.com/texttheater/bach/grammar"
+
+	"testing"
+)
+
+func TestFilter(t *testing.T) {
+	parser, err := participle.Build(
+		&grammar.Filter{},
+		participle.Lexer(grammar.LexerDefinition),
+		participle.Unquote("Str"),
+		participle.Map(grammar.ToKeyword, "Prop"),
+		participle.UseLookahead(0),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	filter := &grammar.Filter{}
+	err = parser.ParseString("each if %2 >0 elis Str all", filter)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
