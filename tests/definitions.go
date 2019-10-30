d230e0c0a8b818f8bff05239ef676d57b8f70670
Kilian Evang <noreply@texttheater.net>
2019-10-30 20:31:48 +0100

    Venturing further in typevar test territory

--- tests/definitions.go
+++ tests/definitions.go
@@ -3,6 +3,7 @@ package tests
 import (
 	"testing"
 
+	"github.com/texttheater/bach/errors"
 	"github.com/texttheater/bach/types"
 	"github.com/texttheater/bach/values"
 )
@@ -48,6 +49,29 @@ func TestDefinitions(t *testing.T) {
 	TestProgram(`for <A> def apply(f for <A> <B>) <B> as f ok 1 apply(+1)`,
 		types.NumType{},
 		values.NumValue(2),
+		nil,
+		t,
+	)
+	TestProgram(`for <A>|Null def must <A> as is Null then reject else id ok ok null must`,
+		types.TypeVariable{
+			Name: "A",
+		},
+		nil,
+		errors.E(
+			errors.Code(errors.UnexpectedValue),
+			errors.GotValue(values.NullValue{}),
+		),
+		t,
+	)
+	TestProgram(`for <A>|Null def must <A> as is Null then reject else id ok ok 1 must`,
+		types.NumType{},
+		values.NumValue(1),
+		nil,
+		t,
+	)
+	TestProgram(`for <A>|Null def must <A> as is <A> then id else reject ok ok null must`,
+		types.NullType{},
+		values.NullValue{},
 		nil,
 		t,
 	)
