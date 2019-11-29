1cb18becb65776346c2ed80bd608faaea8f48981
Kilian Evang <noreply@texttheater.net>
2019-11-29 23:13:00 +0100

    Added channel-based wrappers around lazy arrays
    More ergonomical for implementing mappings, built-in funcers etc.

--- states/thunks.go
+++ states/thunks.go
@@ -41,3 +41,48 @@ func ThunkFromState(state State) *Thunk {
 		},
 	}
 }
+
+func ChannelFromValue(value Value) <-chan Result {
+	channel := make(chan Result)
+	go func() {
+		defer close(channel)
+		arr := value.(*ArrValue)
+		for arr != nil {
+			channel <- Result{
+				State: State{
+					Value: arr.Head,
+				},
+			}
+			var err error
+			arr, err = arr.GetTail()
+			if err != nil {
+				channel <- Result{
+					Error: err,
+				}
+			}
+		}
+	}()
+	return channel
+}
+
+func ThunkFromChannel(channel <-chan Result) *Thunk {
+	var next func() *Thunk
+	next = func() *Thunk {
+		res, ok := <-channel
+		if !ok {
+			return ThunkFromValue((*ArrValue)(nil))
+		}
+		if res.Error != nil {
+			return ThunkFromError(res.Error)
+		}
+		return ThunkFromValue(&ArrValue{
+			Head: res.State.Value,
+			Tail: &Thunk{
+				Func: func() *Thunk {
+					return next()
+				},
+			},
+		})
+	}
+	return next()
+}
