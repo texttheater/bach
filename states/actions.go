5a5c21d0a93a1850891ff09bebdeae014f0bb4c8
Kilian Evang <noreply@texttheater.net>
2019-12-18 09:34:25 +0100

    Removed unused SetArg method

--- states/actions.go
+++ states/actions.go
@@ -2,21 +2,6 @@ package states
 
 type Action func(inputState State, args []Action) *Thunk
 
-// TODO remove SetArg; currently one builtin funcer uses it; we should provide
-// a better abstraction to builtin funcers making sure that partial application
-// is handled correctly.
-
-func (a Action) SetArg(arg Action) Action {
-	return func(inputState State, outerArgs []Action) *Thunk {
-		args := make([]Action, len(outerArgs)+1)
-		args[0] = arg
-		for i, outerArg := range outerArgs {
-			args[i+1] = outerArg
-		}
-		return a(inputState, args)
-	}
-}
-
 func SimpleAction(value Value) Action {
 	return func(inputState State, args []Action) *Thunk {
 		return ThunkFromState(State{
