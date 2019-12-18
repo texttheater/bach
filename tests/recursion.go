308abc1055a7d04ef32af626bc89652a9f327101
Kilian Evang <noreply@texttheater.net>
2019-12-18 09:34:02 +0100

    Removed obsolete comment

--- tests/recursion.go
+++ tests/recursion.go
@@ -43,6 +43,7 @@ func TestRecursion(t *testing.T) {
 		t,
 	)
 	// tail-recursive factorial
+	// This does not exhaust the goroutine stack and runs in constant space.
 	TestProgram(
 		`for Num def fac(acc Num) Num as =n if ==0 then acc else acc *n =acc n -1 fac(acc) ok ok 3 fac(1)`,
 		types.NumType{},
@@ -50,19 +51,6 @@ func TestRecursion(t *testing.T) {
 		nil,
 		t,
 	)
-	// same, but with large input
-	// this does not exhaust the goroutine stack
-	// FIXME but it does seem to have a memory leak
-	// (on the heap, then?)
-	// it should be running in constant space
-	// commented out because it's long-running
-	//TestProgram(
-	//	`for Num def fac(acc Num) Num as =n if ==0 then acc else acc *n =acc n -1 fac(acc) ok ok 10000000 fac(1)`,
-	//	types.NumType{},
-	//	states.NumValue(6),
-	//	nil,
-	//	t,
-	//)
 	// fold for numbers
 	TestProgram(`for Arr<Num> def fold(start Num, for Num op(Num) Num) Num as is [head;tail] then start op(head) =newStart tail fold(newStart, op) else start ok ok [1, 2, 3] fold(0, +)`,
 		types.NumType{},
