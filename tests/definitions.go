b2b790ffe72d47a6d7864fefeb70d7acb57bf04c
Kilian Evang <noreply@texttheater.net>
2020-01-05 07:23:09 +0100

    Tests formatting

--- tests/definitions.go
+++ tests/definitions.go
@@ -16,55 +16,64 @@ func TestDefinitions(t *testing.T) {
 		nil,
 		t,
 	)
-	TestProgram(`for Num def plusOne Num as +1 ok 1 plusOne plusOne`,
+	TestProgram(
+		`for Num def plusOne Num as +1 ok 1 plusOne plusOne`,
 		types.NumType{},
 		states.NumValue(3),
 		nil,
 		t,
 	)
-	TestProgram(`for Num def apply(for Num f Num) Num as f ok 1 apply(+1)`,
+	TestProgram(
+		`for Num def apply(for Num f Num) Num as f ok 1 apply(+1)`,
 		types.NumType{},
 		states.NumValue(2),
 		nil,
 		t,
 	)
-	TestProgram(`for Num def connectSelf(for Num f(for Any Num) Num) Num as =x f(x) ok 1 connectSelf(+)`,
+	TestProgram(
+		`for Num def connectSelf(for Num f(for Any Num) Num) Num as =x f(x) ok 1 connectSelf(+)`,
 		types.NumType{},
 		states.NumValue(2),
 		nil,
 		t,
 	)
-	TestProgram(`for Num def connectSelf(for Num f(for Any Num) Num) Num as =x f(x) ok 1 connectSelf(+) 3 connectSelf(*)`,
+	TestProgram(
+		`for Num def connectSelf(for Num f(for Any Num) Num) Num as =x f(x) ok 1 connectSelf(+) 3 connectSelf(*)`,
 		types.NumType{},
 		states.NumValue(9),
 		nil,
 		t,
 	)
-	TestProgram(`for Num def connectSelf(for Num f(Num) Num) Num as =x f(x) ok 1 connectSelf(+)`,
+	TestProgram(
+		`for Num def connectSelf(for Num f(Num) Num) Num as =x f(x) ok 1 connectSelf(+)`,
 		types.NumType{},
 		states.NumValue(2),
 		nil,
 		t,
 	)
-	TestProgram(`for Num def apply(for Num f Num) Num as f ok 2 =n apply(+n)`,
+	TestProgram(
+		`for Num def apply(for Num f Num) Num as f ok 2 =n apply(+n)`,
 		types.NumType{},
 		states.NumValue(4),
 		nil,
 		t,
 	)
-	TestProgram(`for Any def f(a Num, b Num) Tup<Num, Num> as [a, b] ok f(2, 3)`,
+	TestProgram(
+		`for Any def f(a Num, b Num) Tup<Num, Num> as [a, b] ok f(2, 3)`,
 		types.TupType([]types.Type{types.NumType{}, types.NumType{}}),
 		states.NewArrValue([]states.Value{states.NumValue(2), states.NumValue(3)}),
 		nil,
 		t,
 	)
-	TestProgram(`for <A> def apply(for <A> f <B>) <B> as f ok 1 apply(+1)`,
+	TestProgram(
+		`for <A> def apply(for <A> f <B>) <B> as f ok 1 apply(+1)`,
 		types.NumType{},
 		states.NumValue(2),
 		nil,
 		t,
 	)
-	TestProgram(`for <A>|Null def myMust <A> as is Null then fatal else id ok ok null myMust`,
+	TestProgram(
+		`for <A>|Null def myMust <A> as is Null then fatal else id ok ok null myMust`,
 		types.TypeVariable{
 			Name: "A",
 		},
@@ -75,13 +84,15 @@ func TestDefinitions(t *testing.T) {
 		),
 		t,
 	)
-	TestProgram(`for <A>|Null def myMust <A> as is Null then fatal else id ok ok 1 myMust`,
+	TestProgram(
+		`for <A>|Null def myMust <A> as is Null then fatal else id ok ok 1 myMust`,
 		types.NumType{},
 		states.NumValue(1),
 		nil,
 		t,
 	)
-	TestProgram(`for <A>|Null def myMust <A> as is <A> then id else fatal ok ok null myMust`,
+	TestProgram(
+		`for <A>|Null def myMust <A> as is <A> then id else fatal ok ok null myMust`,
 		types.NullType{},
 		states.NullValue{},
 		nil,
