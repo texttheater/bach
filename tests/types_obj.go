68f24c05cb81574ae6589fa702106d434ab0006c
Kilian Evang <noreply@texttheater.net>
2020-03-31 17:31:52 +0200

    Code formatting

--- tests/types_obj.go
+++ tests/types_obj.go
@@ -8,11 +8,46 @@ import (
 )
 
 func TestObjectTypes(t *testing.T) {
-	TestProgram(`{} type`, types.StrType{}, states.StrValue("Obj<>"), nil, t)
-	TestProgram(`{a: null} type`, types.StrType{}, states.StrValue("Obj<a: Null>"), nil, t)
-	TestProgram(`{b: false, a: null} type`, types.StrType{}, states.StrValue("Obj<a: Null, b: Bool>"), nil, t)
-	TestProgram(`{c: 0, b: false, a: null} type`, types.StrType{}, states.StrValue("Obj<a: Null, b: Bool, c: Num>"), nil, t)
-	TestProgram(`{d: "", c: 0, b: false, a: null} type`, types.StrType{}, states.StrValue("Obj<a: Null, b: Bool, c: Num, d: Str>"), nil, t)
-	TestProgram(`{e: [], d: "", c: 0, b: false, a: null} type`, types.StrType{}, states.StrValue("Obj<a: Null, b: Bool, c: Num, d: Str, e: Tup<>>"), nil, t)
-	TestProgram(`{f: {}, e: [], d: "", c: 0, b: false, a: null} type`, types.StrType{}, states.StrValue("Obj<a: Null, b: Bool, c: Num, d: Str, e: Tup<>, f: Obj<>>"), nil, t)
+	TestProgram(`{} type`,
+		types.StrType{},
+		states.StrValue("Obj<>"),
+		nil,
+		t,
+	)
+	TestProgram(`{a: null} type`,
+		types.StrType{},
+		states.StrValue("Obj<a: Null>"),
+		nil,
+		t,
+	)
+	TestProgram(`{b: false, a: null} type`,
+		types.StrType{},
+		states.StrValue("Obj<a: Null, b: Bool>"),
+		nil,
+		t,
+	)
+	TestProgram(`{c: 0, b: false, a: null} type`,
+		types.StrType{},
+		states.StrValue("Obj<a: Null, b: Bool, c: Num>"),
+		nil,
+		t,
+	)
+	TestProgram(`{d: "", c: 0, b: false, a: null} type`,
+		types.StrType{},
+		states.StrValue("Obj<a: Null, b: Bool, c: Num, d: Str>"),
+		nil,
+		t,
+	)
+	TestProgram(`{e: [], d: "", c: 0, b: false, a: null} type`,
+		types.StrType{},
+		states.StrValue("Obj<a: Null, b: Bool, c: Num, d: Str, e: Tup<>>"),
+		nil,
+		t,
+	)
+	TestProgram(`{f: {}, e: [], d: "", c: 0, b: false, a: null} type`,
+		types.StrType{},
+		states.StrValue("Obj<a: Null, b: Bool, c: Num, d: Str, e: Tup<>, f: Obj<>>"),
+		nil,
+		t,
+	)
 }
