81ad5b45577eef91e82238996c245458421ae847
Kilian Evang <noreply@texttheater.net>
2020-07-20 17:59:42 +0200

    Reimplemented `id` as simple funcer

--- builtin/values.go
+++ builtin/values.go
@@ -4,25 +4,27 @@ import (
 	"strconv"
 
 	"github.com/texttheater/bach/functions"
-	"github.com/texttheater/bach/parameters"
 	"github.com/texttheater/bach/states"
 	"github.com/texttheater/bach/types"
 )
 
 func initValues() {
 	InitialShape.Stack = InitialShape.Stack.PushAll([]functions.Funcer{
-		func(gotInputShape functions.Shape, gotCall functions.CallExpression, gotParams []*parameters.Parameter) (functions.Shape, states.Action, *states.IDStack, bool, error) {
-			if len(gotCall.Args)+len(gotParams) != 0 {
-				return functions.Shape{}, nil, nil, false, nil
-			}
-			if gotCall.Name != "id" {
-				return functions.Shape{}, nil, nil, false, nil
-			}
-			action := func(inputState states.State, args []states.Action) *states.Thunk {
-				return states.ThunkFromState(inputState)
-			}
-			return gotInputShape, action, nil, true, nil
-		},
+		functions.SimpleFuncer(
+			types.TypeVariable{
+				Name:       "A",
+				UpperBound: types.AnyType{},
+			},
+			"id",
+			nil,
+			types.TypeVariable{
+				Name:       "A",
+				UpperBound: types.AnyType{},
+			},
+			func(inputValue states.Value, argValues []states.Value) (states.Value, error) {
+				return inputValue, nil
+			},
+		),
 		functions.SimpleFuncer(
 			types.StrType{},
 			"parseFloat",
