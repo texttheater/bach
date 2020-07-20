08683e6c36fbb4f4ff80bda6cc194d519462ee09
Kilian Evang <noreply@texttheater.net>
2020-07-20 10:32:11 +0200

    `must` funcer

--- builtin/control.go
+++ builtin/control.go
@@ -35,5 +35,55 @@ func initControl() {
 			}
 			return outputShape, action, nil, true, nil
 		},
+		func(gotInputShape functions.Shape, gotCall functions.CallExpression, gotParams []*parameters.Parameter) (functions.Shape, states.Action, *states.IDStack, bool, error) {
+			typeVar := types.TypeVariable{
+				Name:       "A",
+				UpperBound: types.AnyType{},
+			}
+			wantInputType := types.Union(
+				types.ObjType{
+					PropTypeMap: map[string]types.Type{
+						"just": typeVar,
+					},
+					RestType: types.AnyType{},
+				},
+				types.NullType{},
+			)
+			bindings := make(map[string]types.Type)
+			if !wantInputType.Bind(gotInputShape.Type, bindings) {
+				return functions.Shape{}, nil, nil, false, nil
+			}
+			if len(gotParams) != 0 {
+				return functions.Shape{}, nil, nil, false, nil
+			}
+			if len(gotCall.Args) != 0 {
+				return functions.Shape{}, nil, nil, false, nil
+			}
+			if gotCall.Name != "must" {
+				return functions.Shape{}, nil, nil, false, nil
+			}
+			outputShape := functions.Shape{
+				Type:  bindings["A"],
+				Stack: gotInputShape.Stack,
+			}
+			action := func(inputState states.State, args []states.Action) *states.Thunk {
+				switch v := inputState.Value.(type) {
+				case states.ObjValue:
+					res := v["just"].Eval()
+					if res.Error != nil {
+						return states.ThunkFromError(res.Error)
+					}
+					return states.ThunkFromValue(res.Value)
+				default:
+					return states.ThunkFromError(
+						errors.E(
+							errors.Code(errors.UnexpectedValue),
+							errors.Pos(gotCall.Pos),
+							errors.GotValue(inputState.Value)),
+					)
+				}
+			}
+			return outputShape, action, nil, true, nil
+		},
 	})
 }
