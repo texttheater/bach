25ef886467ec3162a16397318ec918150b801153
Kilian Evang <noreply@texttheater.net>
2020-07-22 12:57:29 +0200

    Build parser *once*

--- grammar/parser.go
+++ grammar/parser.go
@@ -6,25 +6,24 @@ import (
 	"github.com/texttheater/bach/functions"
 )
 
-func Parse(input string) (functions.Expression, error) {
-	parser, err := participle.Build(
+var parser *participle.Parser
+
+func init() {
+	var err error
+	parser, err = participle.Build(
 		&Composition{},
 		participle.Lexer(LexerDefinition),
 		participle.Unquote("Str"),
 		participle.Map(ToKeyword, "Lid"),
 	)
 	if err != nil {
-		if parserError, ok := err.(participle.Error); ok {
-			return nil, errors.E(
-				errors.Code(errors.Syntax),
-				errors.Pos(parserError.Token().Pos),
-				errors.Message(parserError.Message()))
-
-		}
-		return nil, err
+		panic(err)
 	}
+}
+
+func Parse(input string) (functions.Expression, error) {
 	composition := &Composition{}
-	err = parser.ParseString(input, composition)
+	err := parser.ParseString(input, composition)
 	if err != nil {
 		if parserError, ok := err.(participle.Error); ok {
 			return nil, errors.E(
