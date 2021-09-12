433866c26240e93abb498b36d94201f45d78c77b
Kilian Evang <noreply@texttheater.net>
2021-09-12 08:47:03 +0200

    Added param # check to identity expression

--- expressions/identity.go
+++ expressions/identity.go
@@ -2,6 +2,7 @@ package expressions
 
 import (
 	"github.com/alecthomas/participle/lexer"
+	"github.com/texttheater/bach/errors"
 	"github.com/texttheater/bach/parameters"
 	"github.com/texttheater/bach/states"
 )
@@ -15,6 +16,12 @@ func (x IdentityExpression) Position() lexer.Position {
 }
 
 func (x IdentityExpression) Typecheck(inputShape Shape, params []*parameters.Parameter) (Shape, states.Action, *states.IDStack, error) {
+	if len(params) > 0 {
+		return Shape{}, nil, nil, errors.E(
+			errors.Code(errors.ParamsNotAllowed),
+			errors.Pos(x.Pos),
+		)
+	}
 	return inputShape, func(inputState states.State, args []states.Action) *states.Thunk {
 		return states.ThunkFromState(inputState)
 	}, nil, nil
