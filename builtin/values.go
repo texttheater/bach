0b780e5d9b7e4ea2c33e481580be1259135ee56e
Kilian Evang <noreply@texttheater.net>
2022-02-20 18:19:00 +0100

    Added parseJSON funcer (no tests yet)

--- builtin/values.go
+++ builtin/values.go
@@ -1,8 +1,11 @@
 package builtin
 
 import (
+	"encoding/json"
 	"strconv"
 
+	"github.com/alecthomas/participle/lexer"
+	"github.com/texttheater/bach/errors"
 	"github.com/texttheater/bach/expressions"
 	"github.com/texttheater/bach/states"
 	"github.com/texttheater/bach/types"
@@ -65,5 +68,58 @@ func initValues() {
 				return states.NumValue(n), nil
 			},
 		),
+		expressions.RegularFuncer(
+			types.Str{},
+			"parseJSON",
+			nil,
+			types.Any{},
+			func(inputState states.State, args []states.Action, bindings map[string]types.Type, pos lexer.Position) *states.Thunk {
+				str := inputState.Value.(states.StrValue)
+				var data interface{}
+				err := json.Unmarshal([]byte(str), &data)
+				if err != nil {
+					return states.ThunkFromError(err)
+				}
+				return thunkFromData(data)
+			},
+			nil,
+		),
 	})
+}
+
+func thunkFromData(data interface{}) *states.Thunk {
+	switch data := data.(type) {
+	case nil:
+		return states.ThunkFromValue(states.NullValue{})
+	case bool:
+		return states.ThunkFromValue(states.BoolValue(data))
+	case float64:
+		return states.ThunkFromValue(states.NumValue(data))
+	case string:
+		return states.ThunkFromValue(states.StrValue(data))
+	case []interface{}:
+		i := 0
+		iter := func() (states.Value, bool, error) {
+			if i >= len(data) {
+				return nil, false, nil
+			}
+			res := thunkFromData(data[i]).Eval()
+			if res.Error != nil {
+				return nil, false, res.Error
+			}
+			i += 1
+			return res.Value, true, nil
+		}
+		return states.ThunkFromIter(iter)
+	case map[string]interface{}:
+		obj := make(map[string]*states.Thunk)
+		for k, v := range data {
+			obj[k] = thunkFromData(v)
+		}
+		return states.ThunkFromValue(states.ObjValue(obj))
+	default:
+		return states.ThunkFromError(errors.ValueError(
+			errors.Message("encountered unexpected object while converting from JSON"),
+		))
+	}
 }
