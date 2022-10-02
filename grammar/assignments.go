4c7622a2b98061775c3af48ff801b587d23a0c11
Kilian Evang <noreply@texttheater.net>
2022-10-02 13:45:38 +0200

    Simplified Assignment grammar

--- grammar/assignments.go
+++ grammar/assignments.go
@@ -7,22 +7,10 @@ import (
 
 type Assignment struct {
 	Pos    lexer.Position
-	EqName *EqName `@@`
-}
-
-func (g *Assignment) Ast() (expressions.Expression, error) {
-	if g.EqName != nil {
-		return g.EqName.Ast()
-	}
-	panic("invalid assignment")
-}
-
-type EqName struct {
-	Pos    lexer.Position
 	EqName string `@EqName`
 }
 
-func (g *EqName) Ast() (expressions.Expression, error) {
+func (g *Assignment) Ast() (expressions.Expression, error) {
 	name := g.EqName[1:]
 	return &expressions.AssignmentExpression{
 		Pos:  g.Pos,
