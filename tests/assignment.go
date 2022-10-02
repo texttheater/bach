90db7afbadd2e2c20bfd7c34e900162718365818
Kilian Evang <noreply@texttheater.net>
2022-10-02 14:39:31 +0200

    Assignment tests now use TestProgramStr

--- tests/assignment.go
+++ tests/assignment.go
@@ -2,12 +2,21 @@ package tests
 
 import (
 	"testing"
-
-	"github.com/texttheater/bach/states"
-	"github.com/texttheater/bach/types"
 )
 
 func TestAssignment(t *testing.T) {
-	TestProgram("1 +1 =a 3 *2 +a", types.Num{}, states.NumValue(8), nil, t)
-	TestProgram("1 +1 ==2 =p 1 +1 ==1 =q p ==q not", types.Bool{}, states.BoolValue(true), nil, t)
+	TestProgramStr(
+		`1 +1 =a 3 *2 +a`,
+		`Num`,
+		`8`,
+		nil,
+		t,
+	)
+	TestProgramStr(
+		`1 +1 ==2 =p 1 +1 ==1 =q p ==q not`,
+		`Bool`,
+		`true`,
+		nil,
+		t,
+	)
 }
