dc05d5667bf73005493388a2d68b95714060cffa
Kilian Evang <noreply@texttheater.net>
2024-01-05 17:20:18 +0100

    Removed the concept of "funcer"
    Everything is now done in the loop in CallExpression.Typecheck, with no inner
    helper function.

--- expressions/call.go
+++ expressions/call.go
@@ -24,6 +24,7 @@ func (x CallExpression) Typecheck(inputShape Shape, p []*params.Param) (Shape, s
 	// go down the function stack and find the function invoked by this
 	// call
 	stack := inputShape.Stack
+funcers:
 	for {
 		// reached bottom of stack without finding a matching funcer
 		if stack == nil {
@@ -40,114 +41,107 @@ func (x CallExpression) Typecheck(inputShape Shape, p []*params.Param) (Shape, s
 		var funOutputShape Shape
 		var funAction3 states.Action
 		ids := funcerDefinition.IDs
-		funcer := func(gotInputShape Shape, gotCall CallExpression, gotParams []*params.Param) (Shape, states.Action, *states.IDStack, bool, error) {
-			// match number of parameters
-			if len(x.Args)+len(p) != len(funcerDefinition.Params) {
-				return Shape{}, nil, nil, false, nil
-			}
-			// match name
-			if x.Name != funcerDefinition.Name {
-				return Shape{}, nil, nil, false, nil
-			}
-			// match input type
-			bindings := make(map[string]types.Type)
-			if !funcerDefinition.InputType.Bind(inputShape.Type, bindings) {
-				return Shape{}, nil, nil, false, nil
-			}
-			if !funcerDefinition.InputType.Instantiate(bindings).Subsumes(inputShape.Type) {
-				return Shape{}, nil, nil, false, nil
-			}
-			// typecheck and set parameters filled by this call
-			funAction := func(inputState states.State, args []states.Action) *states.Thunk {
-				return funcerDefinition.Kernel(inputState, args, bindings, x.Position())
-			}
-			argActions := make([]states.Action, len(x.Args))
-			argIDss := make([]*states.IDStack, len(x.Args))
-			for i := range x.Args {
-				argInputShape := Shape{
-					Type: funcerDefinition.Params[i].InputType.Instantiate(bindings),
-					// TODO what if we don't have the bindings yet?
-					// TODO what if an incompatible bound is declared?
-					Stack: inputShape.Stack,
-				}
-				argOutputShape, argAction, argIDs, err := x.Args[i].Typecheck(
-					argInputShape, instantiate(funcerDefinition.Params[i].Params, bindings))
-				if err != nil {
-					return Shape{}, nil, nil, false, nil
-				}
-				if !funcerDefinition.Params[i].OutputType.Bind(argOutputShape.Type, bindings) {
-					return Shape{}, nil, nil, false, errors.TypeError(
-						errors.Code(errors.ArgHasWrongOutputType),
-						errors.Pos(x.Pos),
-						errors.ArgNum(i+1),
-						errors.WantType(funcerDefinition.Params[i].OutputType.Instantiate(bindings)),
-						errors.GotType(argOutputShape.Type),
-					)
-				}
-				if !funcerDefinition.Params[i].OutputType.Instantiate(bindings).Subsumes(argOutputShape.Type) {
-					return Shape{}, nil, nil, false, errors.TypeError(
-						errors.Code(errors.ArgHasWrongOutputType),
-						errors.Pos(x.Pos),
-						errors.ArgNum(i+1),
-						errors.WantType(funcerDefinition.Params[i].OutputType.Instantiate(bindings)),
-						errors.GotType(argOutputShape.Type),
-					)
-				}
-				argActions[i] = argAction
-				argIDss[i] = argIDs
-				ids = ids.AddAll(argIDs)
-			}
-			// pass input variable stack to arguments
-			funAction2 := func(inputState states.State, args []states.Action) *states.Thunk {
-				args2 := make([]states.Action, len(argActions)+len(args))
-				for i := range argActions {
-					prunedStack := inputState.Stack.Keep(argIDss[i]) // pass only what is needed to avoid memory leak
-					i := i
-					args2[i] = func(argInputState states.State, argArgs []states.Action) *states.Thunk {
-						argInputState.Stack = prunedStack
-						return argActions[i](argInputState, argArgs)
-					}
-				}
-				for i := 0; i < len(args); i++ {
-					args2[len(argActions)+i] = args[i]
-				}
-				return funAction(inputState, args2)
-			}
-			// typecheck parameters not filled by the call
-			for i, gotParam := range p {
-				wantParam := funcerDefinition.Params[len(x.Args)+i].Instantiate(bindings)
-				if !gotParam.Subsumes(wantParam) {
-					return Shape{}, nil, nil, false, errors.TypeError(
-						errors.Code(errors.ParamDoesNotMatch),
-						errors.Pos(x.Pos),
-						errors.ParamNum(i+1),
-						errors.WantParam(gotParam),
-						errors.GotParam(wantParam),
-					)
-				}
-			}
-			// create output shape
-			funOutputShape = Shape{
-				Type:  funcerDefinition.OutputType.Instantiate(bindings),
+		// match number of parameters
+		if len(x.Args)+len(p) != len(funcerDefinition.Params) {
+			stack = stack.Tail
+			continue funcers
+		}
+		// match name
+		if x.Name != funcerDefinition.Name {
+			stack = stack.Tail
+			continue funcers
+		}
+		// match input type
+		bindings := make(map[string]types.Type)
+		if !funcerDefinition.InputType.Bind(inputShape.Type, bindings) {
+			stack = stack.Tail
+			continue funcers
+		}
+		if !funcerDefinition.InputType.Instantiate(bindings).Subsumes(inputShape.Type) {
+			stack = stack.Tail
+			continue funcers
+		}
+		// typecheck and set parameters filled by this call
+		funAction := func(inputState states.State, args []states.Action) *states.Thunk {
+			return funcerDefinition.Kernel(inputState, args, bindings, x.Position())
+		}
+		argActions := make([]states.Action, len(x.Args))
+		argIDss := make([]*states.IDStack, len(x.Args))
+		for i := range x.Args {
+			argInputShape := Shape{
+				Type: funcerDefinition.Params[i].InputType.Instantiate(bindings),
+				// TODO what if we don't have the bindings yet?
+				// TODO what if an incompatible bound is declared?
 				Stack: inputShape.Stack,
 			}
-			// set new type variables on action
-			funAction3 = func(inputState states.State, args []states.Action) *states.Thunk {
-				for n, t := range bindings {
-					inputState.TypeStack = inputState.TypeStack.Update(n, t)
-				}
-				return funAction2(inputState, args)
+			argOutputShape, argAction, argIDs, err := x.Args[i].Typecheck(
+				argInputShape, instantiate(funcerDefinition.Params[i].Params, bindings))
+			if err != nil {
+				stack = stack.Tail
+				continue funcers
 			}
-			// return
-			return funOutputShape, funAction3, ids, true, nil
+			if !funcerDefinition.Params[i].OutputType.Bind(argOutputShape.Type, bindings) {
+				return Shape{}, nil, nil, errors.TypeError(
+					errors.Code(errors.ArgHasWrongOutputType),
+					errors.Pos(x.Pos),
+					errors.ArgNum(i+1),
+					errors.WantType(funcerDefinition.Params[i].OutputType.Instantiate(bindings)),
+					errors.GotType(argOutputShape.Type),
+				)
+			}
+			if !funcerDefinition.Params[i].OutputType.Instantiate(bindings).Subsumes(argOutputShape.Type) {
+				return Shape{}, nil, nil, errors.TypeError(
+					errors.Code(errors.ArgHasWrongOutputType),
+					errors.Pos(x.Pos),
+					errors.ArgNum(i+1),
+					errors.WantType(funcerDefinition.Params[i].OutputType.Instantiate(bindings)),
+					errors.GotType(argOutputShape.Type),
+				)
+			}
+			argActions[i] = argAction
+			argIDss[i] = argIDs
+			ids = ids.AddAll(argIDs)
 		}
-		_, _, _, ok, err := funcer(inputShape, x, p)
-		if err != nil {
-			return Shape{}, nil, nil, err
+		// pass input variable stack to arguments
+		funAction2 := func(inputState states.State, args []states.Action) *states.Thunk {
+			args2 := make([]states.Action, len(argActions)+len(args))
+			for i := range argActions {
+				prunedStack := inputState.Stack.Keep(argIDss[i]) // pass only what is needed to avoid memory leak
+				i := i
+				args2[i] = func(argInputState states.State, argArgs []states.Action) *states.Thunk {
+					argInputState.Stack = prunedStack
+					return argActions[i](argInputState, argArgs)
+				}
+			}
+			for i := 0; i < len(args); i++ {
+				args2[len(argActions)+i] = args[i]
+			}
+			return funAction(inputState, args2)
 		}
-		if !ok {
-			stack = stack.Tail
-			continue
+		// typecheck parameters not filled by the call
+		for i, gotParam := range p {
+			wantParam := funcerDefinition.Params[len(x.Args)+i].Instantiate(bindings)
+			if !gotParam.Subsumes(wantParam) {
+				return Shape{}, nil, nil, errors.TypeError(
+					errors.Code(errors.ParamDoesNotMatch),
+					errors.Pos(x.Pos),
+					errors.ParamNum(i+1),
+					errors.WantParam(gotParam),
+					errors.GotParam(wantParam),
+				)
+			}
+		}
+		// create output shape
+		funOutputShape = Shape{
+			Type:  funcerDefinition.OutputType.Instantiate(bindings),
+			Stack: inputShape.Stack,
+		}
+		// set new type variables on action
+		funAction3 = func(inputState states.State, args []states.Action) *states.Thunk {
+			for n, t := range bindings {
+				inputState.TypeStack = inputState.TypeStack.Update(n, t)
+			}
+			return funAction2(inputState, args)
 		}
 		// make call lazy
 		action := func(inputState states.State, args []states.Action) *states.Thunk {
