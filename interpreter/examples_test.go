19e94388951b60b8c267cca0750854fa62e30b49
Kilian Evang <noreply@texttheater.net>
2024-01-28 17:11:28 +0100

    One example test function per builtin category

--- interpreter/examples_test.go
+++ interpreter/examples_test.go
@@ -7,10 +7,9 @@ import (
 	"github.com/texttheater/bach/interpreter"
 )
 
-func TestExamples(t *testing.T) {
-	stack := builtin.InitialShape.Stack
-	for stack != nil {
-		for _, example := range stack.Head.Examples {
+func TestExamplesArr(t *testing.T) {
+	for _, funcer := range builtin.ArrFuncers {
+		for _, example := range funcer.Examples {
 			interpreter.TestProgramStr(
 				example.Program,
 				example.OutputType,
@@ -19,6 +18,145 @@ func TestExamples(t *testing.T) {
 				t,
 			)
 		}
-		stack = stack.Tail
+	}
+}
+
+func TestExamplesControl(t *testing.T) {
+	for _, funcer := range builtin.ControlFuncers {
+		for _, example := range funcer.Examples {
+			interpreter.TestProgramStr(
+				example.Program,
+				example.OutputType,
+				example.OutputValue,
+				example.Error,
+				t,
+			)
+		}
+	}
+}
+
+func TestExamplesIO(t *testing.T) {
+	for _, funcer := range builtin.IOFuncers {
+		for _, example := range funcer.Examples {
+			interpreter.TestProgramStr(
+				example.Program,
+				example.OutputType,
+				example.OutputValue,
+				example.Error,
+				t,
+			)
+		}
+	}
+}
+
+func TestExamplesLogic(t *testing.T) {
+	for _, funcer := range builtin.LogicFuncers {
+		for _, example := range funcer.Examples {
+			interpreter.TestProgramStr(
+				example.Program,
+				example.OutputType,
+				example.OutputValue,
+				example.Error,
+				t,
+			)
+		}
+	}
+}
+
+func TestExamplesMath(t *testing.T) {
+	for _, funcer := range builtin.MathFuncers {
+		for _, example := range funcer.Examples {
+			interpreter.TestProgramStr(
+				example.Program,
+				example.OutputType,
+				example.OutputValue,
+				example.Error,
+				t,
+			)
+		}
+	}
+}
+
+func TestExamplesNull(t *testing.T) {
+	for _, funcer := range builtin.NullFuncers {
+		for _, example := range funcer.Examples {
+			interpreter.TestProgramStr(
+				example.Program,
+				example.OutputType,
+				example.OutputValue,
+				example.Error,
+				t,
+			)
+		}
+	}
+}
+
+func TestExamplesObj(t *testing.T) {
+	for _, funcer := range builtin.ObjFuncers {
+		for _, example := range funcer.Examples {
+			interpreter.TestProgramStr(
+				example.Program,
+				example.OutputType,
+				example.OutputValue,
+				example.Error,
+				t,
+			)
+		}
+	}
+}
+
+func TestExamplesRegexp(t *testing.T) {
+	for _, funcer := range builtin.RegexpFuncers {
+		for _, example := range funcer.Examples {
+			interpreter.TestProgramStr(
+				example.Program,
+				example.OutputType,
+				example.OutputValue,
+				example.Error,
+				t,
+			)
+		}
+	}
+}
+
+func TestExamplesText(t *testing.T) {
+	for _, funcer := range builtin.TextFuncers {
+		for _, example := range funcer.Examples {
+			interpreter.TestProgramStr(
+				example.Program,
+				example.OutputType,
+				example.OutputValue,
+				example.Error,
+				t,
+			)
+		}
+	}
+}
+
+func TestExamplesTypes(t *testing.T) {
+	for _, funcer := range builtin.TypeFuncers {
+		for _, example := range funcer.Examples {
+			interpreter.TestProgramStr(
+				example.Program,
+				example.OutputType,
+				example.OutputValue,
+				example.Error,
+				t,
+			)
+		}
+	}
+}
+
+func TestExamplesValues(t *testing.T) {
+	for _, funcer := range builtin.ValueFuncers {
+		for _, example := range funcer.Examples {
+			interpreter.TestProgramStr(
+				example.Program,
+				example.OutputType,
+				example.OutputValue,
+				example.Error,
+				t,
+			)
+		}
 	}
 }
