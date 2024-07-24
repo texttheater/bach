71da225c5d49c87c0b246d99466cc28a46f7709c
Kilian Evang <noreply@texttheater.net>
2024-07-24 16:11:53 +0200

    Documented logic funcers

--- builtin/logic.go
+++ builtin/logic.go
@@ -9,100 +9,123 @@ import (
 
 var LogicFuncers = []shapes.Funcer{
 	shapes.SimpleFuncer(
-		"",
+		"Returns the value representing logical truth.",
 		types.Any{},
-		"",
+		"any value (is ignored)",
 		"true",
 		nil,
 		types.Bool{},
-		"",
+		"the unique value representing logical truth",
 		"",
 		func(inputValue states.Value, argumentValues []states.Value) (states.Value, error) {
 			return states.BoolValue(true), nil
 		},
-		nil,
+		[]shapes.Example{
+			{`true`, `Bool`, `true`, nil},
+		},
 	),
 	shapes.SimpleFuncer(
-		"",
+		"Returns the value representing logical falsehood.",
 		types.Any{},
-		"",
+		"any value (is ignored)",
 		"false",
 		nil,
 		types.Bool{},
-		"",
+		"the unique value representing logical falsehood",
 		"",
 		func(inputValue states.Value, argumentValues []states.Value) (states.Value, error) {
 			return states.BoolValue(false), nil
 		},
-		nil,
+		[]shapes.Example{
+			{`false`, `Bool`, `false`, nil},
+		},
 	),
 	shapes.SimpleFuncer(
-		"",
+		"Computes logical conjunction.",
 		types.Bool{},
-		"",
+		"a boolean value",
 		"and",
 		[]*params.Param{
-			params.SimpleParam("q", "", types.Bool{}),
+			params.SimpleParam("q", "another boolean value", types.Bool{}),
 		},
 		types.Bool{},
-		"",
+		"true if both the input and q are true, false otherwise",
 		"",
 		func(inputValue states.Value, argumentValues []states.Value) (states.Value, error) {
 			inputBool := inputValue.(states.BoolValue)
 			argumentBool := argumentValues[0].(states.BoolValue)
 			return states.BoolValue(inputBool && argumentBool), nil
 		},
-		nil,
+		[]shapes.Example{
+			{`false and(false)`, `Bool`, `false`, nil},
+			{`false and(true)`, `Bool`, `false`, nil},
+			{`true and(false)`, `Bool`, `false`, nil},
+			{`true and(true)`, `Bool`, `true`, nil},
+		},
 	),
 	shapes.SimpleFuncer(
-		"",
+		"Computes logical disjunction.",
 		types.Bool{},
-		"",
+		"a boolean value",
 		"or",
 		[]*params.Param{
-			params.SimpleParam("q", "", types.Bool{}),
+			params.SimpleParam("q", "another boolean value", types.Bool{}),
 		},
 		types.Bool{},
-		"",
+		"true if at least one of the input and q is true, false otherwise",
 		"",
 		func(inputValue states.Value, argumentValues []states.Value) (states.Value, error) {
 			inputBool := inputValue.(states.BoolValue)
 			argumentBool := argumentValues[0].(states.BoolValue)
 			return states.BoolValue(inputBool || argumentBool), nil
 		},
-		nil,
+		[]shapes.Example{
+			{`false or(false)`, `Bool`, `false`, nil},
+			{`false or(true)`, `Bool`, `true`, nil},
+			{`true or(false)`, `Bool`, `true`, nil},
+			{`true or(true)`, `Bool`, `true`, nil},
+		},
 	),
 	shapes.SimpleFuncer(
-		"",
+		"Computes logical negation.",
 		types.Bool{},
-		"",
+		"a boolean value",
 		"not",
 		nil,
 		types.Bool{},
-		"",
+		"true if the input is false, and false if the input is true",
 		"",
 		func(inputValue states.Value, argumentValues []states.Value) (states.Value, error) {
 			inputBool := inputValue.(states.BoolValue)
 			return states.BoolValue(!inputBool), nil
 		},
-		nil,
+		[]shapes.Example{
+			{`false not`, `Bool`, `true`, nil},
+			{`true not`, `Bool`, `false`, nil},
+			{`1 +1 ==2 and(2 +2 ==5 not)`, `Bool`, `true`, nil},
+		},
 	),
 	shapes.SimpleFuncer(
-		"",
+		"Checks equality of boolean values.",
 		types.Bool{},
-		"",
+		"a boolean value",
 		"==",
 		[]*params.Param{
-			params.SimpleParam("q", "", types.Bool{}),
+			params.SimpleParam("q", "another boolean value", types.Bool{}),
 		},
 		types.Bool{},
-		"",
+		"true if the input and q are identical, false otherwise",
 		"",
 		func(inputValue states.Value, argumentValues []states.Value) (states.Value, error) {
 			inputBool := inputValue.(states.BoolValue)
 			argumentBool := argumentValues[0].(states.BoolValue)
 			return states.BoolValue(inputBool == argumentBool), nil
 		},
-		nil,
+		[]shapes.Example{
+			{`false ==false`, `Bool`, `true`, nil},
+			{`false ==true`, `Bool`, `false`, nil},
+			{`true ==false`, `Bool`, `false`, nil},
+			{`true ==true`, `Bool`, `true`, nil},
+		},
 	),
 }
