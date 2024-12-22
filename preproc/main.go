30df2545b0592fb6defadd45ca5b0b5c0adc6239
Kilian Evang <noreply@texttheater.net>
2024-12-22 20:46:50 +0100

    Dummy preprocessing of book examples

--- preproc/main.go
+++ preproc/main.go
@@ -1,17 +1,46 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"encoding/json"
 	"log"
 	"os"
+	"strings"
 )
 
 type object = map[string]any
 
+func processExampleSet(scanner *bufio.Scanner, buffer *bytes.Buffer) {
+	buffer.WriteString("(example set goes here)\n")
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "```" {
+			return
+		}
+	}
+}
+
+func processExamples(content string) string {
+	reader := strings.NewReader(content)
+	scanner := bufio.NewScanner(reader)
+	var buffer bytes.Buffer
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "```bachdoc" {
+			processExampleSet(scanner, &buffer)
+		} else {
+			buffer.WriteString(line)
+		}
+		buffer.WriteString("\n")
+	}
+	return buffer.String()
+}
+
 func modifyBookItem(item object) {
 	chapter := item["Chapter"].(object)
 	content := chapter["content"].(string)
-	chapter["content"] = content // TODO modify
+	chapter["content"] = processExamples(content)
 	for _, subItem := range chapter["sub_items"].([]any) {
 		modifyBookItem(subItem.(object))
 	}
